87bad96e18eb8a7174ad205d35d278ee9517c6cd
Aleksandar <kaslu1992@gmail.com>
2024-04-10 15:14:25 +0200

    us 1-2 finished

--- src/test/resources/queries/library.sql
+++ src/test/resources/queries/library.sql
@@ -11,16 +11,9 @@ select  id from users;
 -- getAllColumnAsList --> List --> size --> EXPECTED
 -- getAllColumnAsList --> Set  --> size --> EXPECTED
 
+select * from users;
 
-SHOW COLUMNS FROM users;
-SELECT
-    CASE WHEN EXISTS (
-        SELECT 1
-        FROM information_schema.columns
-        WHERE table_name = 'users'
-          AND column_name IN ('id', 'full_name', 'email', 'password', 'user_group_id', 'image', 'extra_data', 'status', 'is_admin', 'start_date', 'end_date', 'address')
-    ) THEN 'Columns found'
-         ELSE 'Columns not found'
-        END AS verification_result;
+
+
 
 
